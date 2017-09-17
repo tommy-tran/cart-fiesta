27d60644bbf4d9209f3c23bb7a81edf814c3e605
tommy-tran <tome7@live.com>
2017-09-16 20:55:02 -0400

    Passing data to recipe-detail component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from "../recipe.model";
 
 @Component({
@@ -7,6 +7,8 @@ import { Recipe } from "../recipe.model";
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeEmit = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe('Mac and Cheese', 'Simply a test', 'http://food.fnr.sndimg.com/content/dam/images/food/fullset/2011/11/14/0/WU-0103_mac-and-cheese_s4x3.jpg.rend.hgtvcom.616.462.suffix/1382541000580.jpeg')
   ];
@@ -14,6 +16,11 @@ export class RecipeListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+  
+  onRecipeSelected(recipe: Recipe) {
+    console.log("Recipe list emits: " + recipe);
+    this.recipeEmit.emit(recipe);
   }
 
 }
