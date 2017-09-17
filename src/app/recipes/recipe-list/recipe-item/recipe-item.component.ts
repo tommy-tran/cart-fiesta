58c47bf8bd88f79bfa2efa274e57403606732f34
tommy-tran <tome7@live.com>
2017-09-16 20:16:51 -0400

    Navigation

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
-
+  
   constructor() { }
 
   ngOnInit() {
